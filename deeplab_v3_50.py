b31535a923f94f912da2b36cc9db07fa91ff9f71
gengyanlei <1599075165@qq.com>
2018-11-26 21:17:06 +0800

    Update deeplab_v3_50.py

--- deeplab_v3_50.py
+++ deeplab_v3_50.py
@@ -49,9 +49,9 @@ class Deeplab_v3(nn.Module):
         self.start=nn.Sequential(encoder.conv1,encoder.bn1,
                                  encoder.relu)
         # (7,2,3)=>(3,2,1)+(3,1,1)+(3,1,1) 修改之处  这里会使网络深，参数减少。
-        #self.start=nn.Sequential(nn.Conv2d(3,64,3,2,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True),\
-        #                         nn.Conv2d(64,64,3,1,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True),\
-        #                         nn.Conv2d(64,64,3,1,1),nn.BatchNorm2d(64),nn.ReLU(inplace=True))
+        #self.start=nn.Sequential(nn.Conv2d(3,64,3,2,1,bias=False),nn.BatchNorm2d(64),nn.ReLU(inplace=True),\
+        #                         nn.Conv2d(64,64,3,1,1,bias=False),nn.BatchNorm2d(64),nn.ReLU(inplace=True),\
+        #                         nn.Conv2d(64,64,3,1,1,bias=False),nn.BatchNorm2d(64),nn.ReLU(inplace=True))
         self.maxpool=encoder.maxpool 
         self.layer1=encoder.layer1
         ##########
