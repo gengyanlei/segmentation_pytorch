83c9fa3e777434e4406ce25a616128ee467a9667
gengyanlei <gengyanlei01@inspur.com>
2020-12-09 10:42:55 +0800

    restart segmentation pytorch

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
-# deeplab_v3
-pytorch deeplab_v3+
+## Semantic Segmentation Pytorch
+```
+    author is leilei
+    Restart this project from 2 years ago 
+```
 
-注意：
-    复现deeplab_v3_plus网络与论文有不同：论文有7个block，此处只是fine-tune pytorch自带的resnet；中间上采样处num_output为48，接在max_pool之后，此处和pytorch resnet有关，因此修改一下。仅供参考，如何重点使用pytorch已经训练的且写好的网络。
+### Environment
+```
+    python: 3.6+
+    ubuntu16.04 or 18.04
+    pytorch 1.6+ (cuda10.2 docker)
+```
+### How to Use
++ Train
 
-注意：这里采用的是resnet基准网络，而论文中展示的是xception网络，但是他也有resnet基准网络。
-此外，输入大小更大的效果会更好，batch_size更大的更好。
++ Test
 
-声明：
-  Pytorch deeplab_v3_plus
-  
-  pytorch : 0.4.0 ； python : 3.5
 
-Pytorch 有自己的可视化工具：visdom  （pip install visdom即可）  
+### Support Network
+- [x] deeplab_v3_plus
 
-pytorch1.1以上版本可以原生支持tensorboard: from torch.utils.tensorboard import summary  
+TODO
+- [ ] pspnet
+- [ ] unet
+- [ ] HRNet
+- [ ] U^2Net
+- [ ] ...
 
-主函数写的一般，没有采用argparse模块。
-
-新增数据增强代码！！！
-
-分享 建筑物语义分割数据集：
-building-segmentation-dataset  
-
-https://github.com/gengyanlei/build_segmentation_dataset
+### others
+* [building-segmentation-dataset](https://github.com/gengyanlei/build_segmentation_dataset) 
+* [reflective-clothes-detect-dataset](https://github.com/gengyanlei/reflective-clothes-detect)
---
+++ models/deeplab_v3_50.py
@@ -0,0 +1,104 @@
+"""
+@author: leilei
+"""
+
+import torch
+import torchvision
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+
+'''
+also fine-tune
+deeplab_v3+ : pytorch resnet 18/34 Basicblock
+                      resnet 50/101/152 Bottleneck
+            this is not original deeplab_v3+, just be based on pytorch's resnet, so many different.
+'''
+class ASPP(nn.Module):
+    # have bias and relu, no bn
+    def __init__(self,in_channel=512,depth=256):
+        super().__init__()
+        # global average pooling : init nn.AdaptiveAvgPool2d ;also forward torch.mean(,,keep_dim=True)
+        self.mean = nn.AdaptiveAvgPool2d((1,1))
+        self.conv = nn.Sequential(nn.Conv2d(in_channel,depth,1,1),nn.ReLU(inplace=True))
+        
+        self.atrous_block1  = nn.Sequential(nn.Conv2d(in_channel,depth,1,1),
+                                           nn.ReLU(inplace=True))
+        self.atrous_block6  = nn.Sequential(nn.Conv2d(in_channel,depth,3,1,padding=6,dilation=6),
+                                            nn.ReLU(inplace=True))
+        self.atrous_block12 = nn.Sequential(nn.Conv2d(in_channel,depth,3,1,padding=12,dilation=12),
+                                            nn.ReLU(inplace=True))
+        self.atrous_block18 = nn.Sequential(nn.Conv2d(in_channel,depth,3,1,padding=18,dilation=18),
+                                            nn.ReLU(inplace=True))
+        
+        self.conv_1x1_output= nn.Sequential(nn.Conv2d(depth*5,depth,1,1),nn.ReLU(inplace=True))
+        
+    def forward(self,x):
+        size = x.shape[2:]
+
+        image_features = self.mean(x)
+        image_features = self.conv(image_features)
+        image_features = F.upsample(image_features,size=size,mode='bilinear',align_corners=True)
+        
+        atrous_block1  = self.atrous_block1(x)
+        
+        atrous_block6  = self.atrous_block6(x)
+        
+        atrous_block12 = self.atrous_block12(x)
+        
+        atrous_block18 = self.atrous_block18(x)
+        
+        net = self.conv_1x1_output(torch.cat([image_features,atrous_block1,atrous_block6,
+                                            atrous_block12,atrous_block18],dim=1))
+        return net
+        
+class Deeplab_v3(nn.Module):
+    # in_channel = 3 fine-tune
+    def __init__(self,class_number=5, fine_tune=True):
+        super().__init__()
+        encoder = torchvision.models.resnet50(pretrained=fine_tune)
+        self.start = nn.Sequential(encoder.conv1,encoder.bn1,
+                                 encoder.relu)
+
+        self.maxpool = encoder.maxpool
+        self.low_feature = nn.Sequential(nn.Conv2d(64,48,1,1),nn.ReLU(inplace=True)) # no bn, has bias and relu
+        
+        self.layer1 = encoder.layer1#256
+        self.layer2 = encoder.layer2#512
+        self.layer3 = encoder.layer3#1024
+        self.layer4 = encoder.layer4#2048
+        
+        self.aspp = ASPP(in_channel=2048, depth=256)
+        
+        self.conv_cat = nn.Sequential(nn.Conv2d(256+48,256,3,1,padding=1),nn.ReLU(inplace=True))
+        self.conv_cat1 = nn.Sequential(nn.Conv2d(256,256,3,1,padding=1),nn.ReLU(inplace=True))
+        self.conv_cat2 = nn.Sequential(nn.Conv2d(256,256,3,1,padding=1),nn.ReLU(inplace=True))
+        self.score = nn.Conv2d(256,class_number,1,1)# no relu and first conv then upsample, reduce memory
+        
+    def forward(self,x):
+        size1 = x.shape[2:]# need upsample input size
+        x  = self.start(x)
+        xm = self.maxpool(x)
+        
+        x = self.layer1(xm)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.aspp(x)
+        
+        low_feature = self.low_feature(xm)
+        size2 = low_feature.shape[2:]
+        decoder_feature = F.upsample(x,size=size2,mode='bilinear',align_corners=True)
+        
+        conv_cat  = self.conv_cat( torch.cat([low_feature,decoder_feature],dim=1) )
+        conv_cat1 = self.conv_cat1(conv_cat)
+        conv_cat2 = self.conv_cat2(conv_cat1)
+        score_small = self.score(conv_cat2)
+        score = F.upsample(score_small,size=size1,mode='bilinear',align_corners=True)
+        
+        return score
+        
+        
+def deeplab_v3_50(class_number=5, fine_tune=True):
+    model=Deeplab_v3(class_number=class_number, fine_tune=fine_tune)
+    return model
